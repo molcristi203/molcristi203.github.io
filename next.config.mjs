6b15ad9638f52879cb9f29b99c030d306c0e8569
molcristi203 <mol.cristi@yahoo.com>
2024-02-24 12:20:48 +0200

    removedd base path

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
     output: "export",
-    basePath: "/molcristi203.github.io",
     images: {
         unoptimized: true
     },
