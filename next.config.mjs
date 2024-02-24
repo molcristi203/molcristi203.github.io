0dfabb40118044a0641db8cddaa6747f693fcbce
molcristi203 <mol.cristi@yahoo.com>
2024-02-24 11:59:56 +0200

    changed config again

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
     output: "export",
     basePath: "/molcristi203.github.io",
     images: {
         unoptimized: true
-    }
+    },
+    assetPrefix: isProd ? "/" : ""
 };
 
 export default nextConfig;
