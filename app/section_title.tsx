c12fdea3aa813bc928f0e972029bd0e7ebc9af62
molcristi203 <mol.cristi@yahoo.com>
2024-02-24 11:41:52 +0200

    solved export errors

--- app/section_title.tsx
+++ app/section_title.tsx
@@ -10,7 +10,8 @@ interface Props {
     backButton : boolean;
 }
 
-const SectionTitle = forwardRef<HTMLDivElement, Props>(({ title, backButton }, ref) => {
+const SectionTitle = forwardRef<HTMLDivElement, Props>(
+    function SectionTitle({title, backButton}, ref) {
     return (
         <div ref={ref} className="sm:p-8 p-4 bh-yellow text-white lg:text-5xl md:text-4xl sm:text-4xl text-2xl border-b-2 border-black flex flex-row flex-nowrap gap-2 items-center" data-navname={title}>
             <Link href="/"><ArrowLeftIcon className={`w-auto lg:h-12 md:h-9 sm:h-9 h-6 ${backButton ? "" : "hidden"}`}/></Link>
