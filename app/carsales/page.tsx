79e9496b84018e170308cbac2eb60dfc6268a47b
molcristi203 <mol.cristi@yahoo.com>
2024-02-29 19:01:58 +0200

    created car sales page

--- app/carsales/page.tsx
+++ app/carsales/page.tsx
@@ -19,6 +19,55 @@ export default function CarSales() {
                     key={1}
                 />
             ]}/>
+            <Section left={[
+                <Image
+                    src={"/images/car_sales/3.png"}
+                    width={2000}
+                    height={2000}
+                    alt="Robotic arm"
+                    className="h-full object-scale-down"
+                    key={2}
+                />
+            ]} right={[
+                <p key={3}>The sign up page where you can create a customer account to buy cars or seller account to list cars for sale.</p>
+            ]} reverse={true}/>
+            <Section left={[
+                <p key={4}>If you already have an account, you can log in into it.</p>
+            ]} right={[
+                <Image
+                    src={"/images/car_sales/2.png"}
+                    width={2000}
+                    height={2000}
+                    alt="Robotic arm"
+                    className="h-full object-scale-down"
+                    key={5}
+                />
+            ]}/>
+            <Section left={[
+                <Image
+                    src={"/images/car_sales/4.png"}
+                    width={2000}
+                    height={2000}
+                    alt="Robotic arm"
+                    className="h-full object-scale-down"
+                    key={6}
+                />
+            ]} right={[
+                <p key={7}>This is the page where you can add cars. It provides input fields for the car details and the posibility to add an image for the listing.</p>
+            ]} reverse={true}/>
+
+            <Section left={[
+                <Image
+                    src={"/images/car_sales/4.png"}
+                    width={2000}
+                    height={2000}
+                    alt="Robotic arm"
+                    className="h-full object-scale-down"
+                    key={8}
+                />
+            ]} right={[
+                <p key={9}>The listing page itself that is accessed when a listing is selected. It contains the photo of the car, the details about the car and the posibility to buy the car if you are logged in as a customer. </p>
+            ]}/>
             <Gallery paths={["/images/car_sales/1.png", "/images/car_sales/2.png", "/images/car_sales/3.png", "/images/car_sales/4.png", "/images/car_sales/5.png"]} />
         </div>
     );
