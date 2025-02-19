f68c17782f2a0b46152ab5f02e3fed1eb3c79ea9
molcristi203 <mol.cristi@yahoo.com>
2025-02-19 15:24:24 +0200

    Added some pages

---
+++ app/road_markings/page.tsx
@@ -0,0 +1,10 @@
+import SectionTitle from "../section_title";
+
+export default function RoadMarkings()
+{
+  return (
+    <div>
+      <SectionTitle title="Road Markings Detection" backButton={true}/>
+    </div>
+  );
+}
